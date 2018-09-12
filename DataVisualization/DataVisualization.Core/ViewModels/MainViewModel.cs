b5d7c4c0e05e58248549ef02d1b4781605e1cbbf
fcin <filip@cinik.eu>
2018-09-12 18:22:07 +0200

    Added very basic menu

--- DataVisualization/DataVisualization.Core/ViewModels/MainViewModel.cs
+++ DataVisualization/DataVisualization.Core/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ namespace DataVisualization.Core.ViewModels
         private readonly IWindowManager _windowManager;
         public VisualizerViewModel VisualizerVm { get; set; }
         public DataBrowserViewModel DataBrowserVm { get; set; }
+        public MenuViewModel MenuVm { get; set; }
 
         public MainViewModel(IWindowManager windowManager, ISeriesFactory seriesFactory, IEventAggregator eventAggregator)
         {
@@ -14,14 +15,10 @@ namespace DataVisualization.Core.ViewModels
 
             VisualizerVm = new VisualizerViewModel(seriesFactory, windowManager, eventAggregator);
             DataBrowserVm = new DataBrowserViewModel(eventAggregator);
+            MenuVm = new MenuViewModel(windowManager);
             ActivateItem(VisualizerVm);
             ActivateItem(DataBrowserVm);
-        }
-
-        public void OnDataLoad()
-        {
-            var dataLoaderVm = new DataLoaderViewModel();
-            _windowManager.ShowDialog(dataLoaderVm);
+            ActivateItem(MenuVm);
         }
     }
 }
---
+++ DataVisualization/DataVisualization.Core/ViewModels/MenuViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+
+namespace DataVisualization.Core.ViewModels
+{
+    public class MenuViewModel : PropertyChangedBase
+    {
+        private readonly IWindowManager _windowManager;
+
+        public MenuViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+
+        public void NewData()
+        {
+            var dataLoaderVm = new DataLoaderViewModel();
+            _windowManager.ShowDialog(dataLoaderVm);
+        }
+    }
+}
