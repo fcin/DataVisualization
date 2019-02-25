468c89b385a0132cd76545c2a623d7752ea9f91c
fcin <filip@cinik.eu>
2019-02-25 19:06:24 +0100

    More validation rules

---
+++ DataVisualization/DataVisualization.Core/ValidationRules/NumericValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace DataVisualization.Core.ValidationRules
+{
+    public class NumericValidationRule : ValidationRule
+    {
+        private const NumberStyles NumericStyles = NumberStyles.Float;
+        private readonly NumberFormatInfo _numericInfo = new NumberFormatInfo();
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return value != null && double.TryParse(value.ToString(), NumericStyles, _numericInfo, out var _) ?
+                ValidationResult.ValidResult :
+                new ValidationResult(false, "Not a number");
+        }
+    }
+}
--- DataVisualization/DataVisualization.Core/Views/SeriesPropertiesView.xaml.cs
+++ DataVisualization/DataVisualization.Core/Views/SeriesPropertiesView.xaml.cs
@@ -15,36 +15,5 @@ namespace DataVisualization.Core.Views
         {
             InitializeComponent();
         }
-
-        private void TryUpdateItemValue(object sender, TextCompositionEventArgs e)
-        {
-            if (!(sender is TextBox textbox))
-                throw new InvalidOperationException();
-
-            var text = textbox.Text.Insert(textbox.SelectionStart, e.Text);
-            if (double.TryParse(text, out var value))
-            {
-                e.Handled = false;
-                return;
-            }
-
-            e.Handled = true;
-        }
-
-        private void TryPasteItemValue(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
-            {
-                var text = (string)e.DataObject.GetData(typeof(string));
-                if (!double.TryParse(text, out var _))
-                {
-                    e.CancelCommand();
-                }
-            }
-            else
-            {
-                e.CancelCommand();
-            }
-        }
     }
 }
